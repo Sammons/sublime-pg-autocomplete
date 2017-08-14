c34cc8d5da473b423aec2a76efbfaca7d96b2b97
Ben Sammons <ben@sammons.io>
2017-08-14 16:56:21 -0400

    Update pg_autocomplete.py

--- pg_autocomplete.py
+++ pg_autocomplete.py
@@ -7,7 +7,7 @@ import json
 #
 # 1) Go to tools -> Developer -> New Plugin ...
 # 2) Overwrite the generated plugin with the contents of this file
-# 3) Save, should save to the User folder under packages
+# 3) Save, should save to the User folder under packages (where plugins reside for st3)
 # 4) Configure your postgres connection via your settings, 
 #    using the settings seen below starting on line 18
 # 5) it slows things way down because it re-fires this query over and over
